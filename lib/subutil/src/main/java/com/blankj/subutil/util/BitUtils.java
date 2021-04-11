7e21a44685d70423b4352a30029d36e1762753ac
YashMaheshBangera <yash.bangera7@gmail.com>
2021-04-11 18:31:55 -0400

    Third A4 - OneDeclarationPerLine - EncryptUtils.rc4() - Correction done

--- lib/subutil/src/main/java/com/blankj/subutil/util/BitUtils.java
+++ lib/subutil/src/main/java/com/blankj/subutil/util/BitUtils.java
@@ -38,6 +38,8 @@ public final class BitUtils {
      * @param pos    指定位置 (0<=pos<=7)
      * @param value  只能取值为 0, 或 1, 所有大于0的值作为1处理, 所有小于0的值作为0处理
      * @return 运算后的结果数
+     * TODO: remove useless parenthesis surrounding ~mask
+     * FIXME: remove the "(" and ")" around ~mask
      */
     public static byte setBitValue(byte source, int pos, byte value) {
 

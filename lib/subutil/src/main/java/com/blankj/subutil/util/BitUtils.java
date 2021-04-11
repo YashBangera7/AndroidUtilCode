bf909f6999bb2671cf2cf4bbf39b7e6cce28b7e6
YashMaheshBangera <yash.bangera7@gmail.com>
2021-04-11 19:49:30 -0400

    Seventh A4 - previous commit message change - AvoidDuplicateLiterals - BitUtils.java - Correction made

--- lib/subutil/src/main/java/com/blankj/subutil/util/BitUtils.java
+++ lib/subutil/src/main/java/com/blankj/subutil/util/BitUtils.java
@@ -87,30 +87,29 @@ public final class BitUtils {
      * 入口函数做测试<br>
      *
      * @param args
-     * TODO: Replace all "BitUtils" string by a single String constant
-     * FIXME: Declare constant String and replace in place of Duplicate Literals
      */
     public static void main(String[] args) {
         // 取十进制 11 (二级制 0000 1011) 为例子
         byte source = 11;
+        String variableBitUtils = "BitUtils";
         // 取第2位值并输出, 结果应为 0000 1011
         for (byte i = 7; i >= 0; i--) {
-            Log.d("BitUtils", getBitValue(source, i) + "");
+            Log.d(variableBitUtils, getBitValue(source, i) + "");
 
         }
         // 将第6位置为1并输出 , 结果为 75 (0100 1011)
-        Log.d("BitUtils", setBitValue(source, 6, (byte) 1) + "");
+        Log.d(variableBitUtils, setBitValue(source, 6, (byte) 1) + "");
 
         // 将第6位取反并输出, 结果应为75(0100 1011)
-        Log.d("BitUtils", reverseBitValue(source, 6) + "");
+        Log.d(variableBitUtils, reverseBitValue(source, 6) + "");
 
         // 检查第6位是否为1，结果应为false
-        Log.d("BitUtils", checkBitValue(source, 6) + "");
+        Log.d(variableBitUtils, checkBitValue(source, 6) + "");
 
         // 输出为1的位, 结果应为 0 1 3
         for (byte i = 0; i < 8; i++) {
             if (checkBitValue(source, i)) {
-                Log.d("BitUtils", i + "");
+                Log.d(variableBitUtils, i + "");
             }
         }
     }
